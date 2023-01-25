2523364ddcfca11ce0997a9123e84129227291b1
IvanoffFrancisco <nicoo.ivanoff@gmail.com>
2023-01-25 01:07:33 -0300

    fix: arreglo de propiedades

--- src/routes/reclamo.routes.js
+++ src/routes/reclamo.routes.js
@@ -5,7 +5,7 @@ const router=Router();
 
 router.get("/api/reclamo/id/:id",async (req,res)=>{
     const {id}=req.params;
-    const [results]=await pool.query("SELECT reclamo.titulo,reclamo.domicilio,reclamo.nombre,reclamo.apellido,reclamo.telefono, reclamo.fecha,reclamo.mensaje,reclamo.id_usuario,reclamo.actuacion_simple,reclamo.division,reclamo.estado, usuario.usuario FROM reclamo INNER JOIN usuario ON reclamo.id_usuario = usuario.id_usuario WHERE id_reclamo=?",[id]);
+    const [results]=await pool.query("SELECT reclamo.titulo,reclamo.domicilio,reclamo.nombre,reclamo.apellido,reclamo.telefono, reclamo.fecha,reclamo.mensaje,reclamo.idUsuario,reclamo.actuacion_simple,reclamo.division,reclamo.estado, usuario.usuario FROM reclamo INNER JOIN usuario ON reclamo.idUsuario = usuario.idUsuario WHERE idReclamo=?",[id]);
     res.json(results);
 });
 router.get("/api/reclamo/:division",async(req,res)=>{
@@ -15,8 +15,8 @@ router.get("/api/reclamo/:division",async(req,res)=>{
 })
 
 router.post("/api/reclamo",async (req,res)=>{
-    const {titulo, domicilio, nombre, apellido,telefono,fecha,mensaje,id_usuario,actuacion_simple,division,estado}=req.body;
-    const [results]=await pool.query("INSERT INTO reclamo(titulo,domicilio,nombre,apellido,telefono,fecha,mensaje,id_usuario, actuacion_simple,division,estado) VALUES (?,?,?,?,?,?,?,?,?,?,?)",[titulo, domicilio, nombre, apellido,telefono,fecha,mensaje,id_usuario,actuacion_simple,division,estado]);
+    const {titulo, domicilio, nombre, apellido,telefono,fecha,mensaje,idUsuario,actuacion_simple,division,estado}=req.body;
+    const [results]=await pool.query("INSERT INTO reclamo(titulo,domicilio,nombre,apellido,telefono,fecha,mensaje,idUsuario,actuacion_simple,division,estado) VALUES (?,?,?,?,?,?,?,?,?,?,?)",[titulo, domicilio, nombre, apellido,telefono,fecha,mensaje,idUsuario,actuacion_simple,division,estado]);
     res.json(results);
 })
 
--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -4,14 +4,14 @@ import {pool} from '../conexionDB.js';
 const router=Router();
 
 router.post("/api/login",async (req,res)=>{
-    const {usuario,contraseña}=req.body;
-    const [results]=await pool.query('SELECT * FROM usuario WHERE usuario=? AND contraseña=?',[usuario,contraseña]);
+    const {usuario,password}=req.body;
+    const [results]=await pool.query('SELECT * FROM usuario WHERE usuario=? AND password=?',[usuario,password]);
     res.json(results);
 });
 
 router.post("/api/registro",async(req,res)=>{
-    const {nombre, apellido, usuario, contraseña, numero_legajo, tipo_de_usuario}=req.body;
-    const [results]=await pool.query("INSERT INTO usuario (nombre, apellido, usuario, contraseña, numero_legajo, tipo_de_usuario) VALUES (?,?,?,?,?,?)",[nombre, apellido, usuario, contraseña, numero_legajo, tipo_de_usuario]);
+    const {nombre, apellido, usuario, password, numeroLegajo, tipoDeUsuario}=req.body;
+    const [results]=await pool.query("INSERT INTO usuario (nombre, apellido, usuario, password, numeroLegajo, tipoDeUsuario) VALUES (?,?,?,?,?,?)",[nombre, apellido, usuario, password, numeroLegajo, tipoDeUsuario]);
     res.json(results);
 })
 
